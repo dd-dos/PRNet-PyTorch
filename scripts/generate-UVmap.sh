b9ee1acf73ae4f4dcd407a143dfb197967b9cef0
dd-dos <dungphamdang99@gmail.com>
2021-05-27 17:26:23 +0700

    Intial commit

--- scripts/generate-UVmap.sh
+++ scripts/generate-UVmap.sh
@@ -1,2 +1,3 @@
-python processor.py -i=data/images/AFLW2000 -o=data/images/AFLW2000-crop -f=True -v=True --isOldKpt=True
+export PYTHONPATH="."
+python data/processor.py -i=data/images/AFLW2000 -o=data/images/AFLW2000-crop -f=True -v=True --isOldKpt=True
 # python processor.py -i=data/images/300W_LP -o=data/images/300W_LP-crop --thread=16
