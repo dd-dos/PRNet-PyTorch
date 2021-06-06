99f8c76c7c8728d1f5209b552ba20f2f81d0b2fa
dd-dos <dungphamdang99@gmail.com>
2021-06-06 02:20:08 +0000

    Fix typo

--- scripts/generate-UVmap.sh
+++ scripts/generate-UVmap.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH="."
-python data/processor.py -i=data/images/AFLW2000 -o=data/images/AFLW2000-crop-ipdb -f=True -v=True --isOldKpt=True
-# python processor.py -i=data/images/300W_LP -o=data/images/300W_LP-crop --thread=16
+python data/processor.py -i=data/images/AFLW2000 -o=data/images/AFLW2000-crop -f=True -v=True --isOldKpt=True --thread=8
+python data/processor.py -i=data/images/300W_LP -o=data/images/300W_LP-crop --thread=8
