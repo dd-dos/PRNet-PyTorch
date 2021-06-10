65065b8432d9b23102cd001b2faf7700842b8cda
pdd-99 <gran@techainer.com>
2021-06-10 22:27:41 +0700

    Increase test size to 128

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@ python3 train.py \
 --train-root "data/images/300W_LP-crop" \
 --test-root "data/images/AFLW2000-crop" \
 --batch-size 128 \
---test-size 64 \
+--test-size 128 \
 --save-path "weights" \
 --num-workers 8 \
 --pretrained "weights/2021-06-08/best.pth"
